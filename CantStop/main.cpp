a1ae27f16012ac47be13df01132f0a3ef10c4274
khadkaashish <ashishkhadka03@gmail.com>
2025-01-29 15:39:43 -0500

    Initial Commit

---
+++ CantStop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CantStop
+//
+//  Created by Ashish Khadka on 1/29/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
