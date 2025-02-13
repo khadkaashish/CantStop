174a751612346e4c47afa434dcbccd09f49d20b3
khadkaashish <ashishkhadka03@gmail.com>
2025-02-13 12:21:27 -0500

    Column Class

---
+++ CantStop/src/Column.cpp
@@ -0,0 +1,8 @@
+//
+//  Column.cpp
+//  CantStop
+//
+//  Created by Ashish Khadka on 2/12/25.
+//
+
+#include "Column.hpp"
---
+++ CantStop/src/Column.hpp
@@ -0,0 +1,13 @@
+//
+//  Column.hpp
+//  CantStop
+//
+//  Created by Ashish Khadka on 2/12/25.
+//
+
+#ifndef Column_hpp
+#define Column_hpp
+
+#include <stdio.h>
+
+#endif /* Column_hpp */
