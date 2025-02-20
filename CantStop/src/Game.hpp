f70b423c3334b8528319124867b1a36294e5317b
khadkaashish <ashishkhadka03@gmail.com>
2025-02-20 16:12:30 -0500

    Changes to Game

--- CantStop/src/Game.hpp
+++ CantStop/src/Game.hpp
@@ -1,31 +1,13 @@
-// ================================================================
-// Name: Can't Stop Board Game        Feb, 2025
-// Author:  Ashish Khadka and Aryan Tandon      File: Game.hpp
-// ================================================================
-#pragma once
-#include "tools.hpp"
-#include "Dice.hpp"
-#include "Player.hpp"
-#include "Column.hpp"
-// -----------------------------------------------------------------
-class Game {
-private:
-	Dice* dice; // Pointer to dice
-	Player player1;
-	Player player2;
-	Column column2;
-	Column column7;	
-public:
-	Game();
-	~Game() { delete dice; }
-	static Player getNewPlayer();
-	static string getName();
-	static ECcolor getColor();
-	ostream& print (ostream& os);
-};
-//  Overload the output operator
-inline ostream& operator<<(ostream& os, Game& game) {
-	return game.print(os);
-}
+//
+//  Game.hpp
+//  CantStop
+//
+//  Created by Ashish Khadka on 2/20/25.
+//
 
+#ifndef Game_hpp
+#define Game_hpp
 
+#include <stdio.h>
+
+#endif /* Game_hpp */
