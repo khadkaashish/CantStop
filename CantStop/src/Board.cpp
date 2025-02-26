afe7845666c1957418f6180342536e1d80a396ed
khadkaashish <ashishkhadka03@gmail.com>
2025-02-26 17:26:43 -0500

    Game and Board

---
+++ CantStop/src/Board.cpp
@@ -0,0 +1,8 @@
+//
+//  Board.cpp
+//  CantStop
+//
+//  Created by Ashish Khadka on 2/25/25.
+//
+
+#include "Board.hpp"
---
+++ CantStop/src/Board.hpp
@@ -0,0 +1,13 @@
+//
+//  Board.hpp
+//  CantStop
+//
+//  Created by Ashish Khadka on 2/25/25.
+//
+
+#ifndef Board_hpp
+#define Board_hpp
+
+#include <stdio.h>
+
+#endif /* Board_hpp */
